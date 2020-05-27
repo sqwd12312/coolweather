4f6127bd80b1b80d2bc442bd93bd0a3a1c471876
sqwd12312 <411319775@qq.com>
2020-05-27 19:55:27 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by qny on 2020/5/27.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
